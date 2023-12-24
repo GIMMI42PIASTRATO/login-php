dfa924a49ab1ba85baa37d30b36f0dabb5569ff1
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-12-24 14:20:15 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <?php require "./components/nav.html" ?>
+    <div class="formContainer">
+        <form method="POST">
+            <div class="form">
+                <h1>Register</h1>
+                <input type="text" name="" id="" placeholder="Name">
+                <input type="email" name="" id="" placeholder="Email">
+                <input type="password" name="" id="" placeholder="Password">
+                <button type="submit">Register now</button>
+                <a href="#">Or login</a>
+            </div>
+        </form>
+    </div>
+    <?php require "./components/footer.html" ?>
+</body>
+</html>
+
+<!-- chipi chipi chapa chapa dubi dubi daba daba magico mi dubi dubi boom boom boom boom -->
